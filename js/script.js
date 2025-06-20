e2d6a4c89c98c8700cdd628cb04aea64bc2909cc
DhaniKWP <dhanikusuma256@gmail.com>
2025-06-20 12:55:25 +0700

    6. menambahkan isi dari pilihan produk

--- js/script.js
+++ js/script.js
@@ -4,7 +4,16 @@
         let currentDiscount = 0;
         let appliedPromoCode = '';
 
-
+        const promoCodes = {
+          'SHOPEE1111': { discount: 5, type: 'percentage', description: 'Selamat anda mendapatkan sebesar Diskon 5%'},
+          'SHOPEE1212': { discount: 10, type: 'percentage', description: 'Selamat anda mendapatkan sebesar Diskon 90%'},
+          'TIKTOK1111': { discount: 10, type: 'percentage', description: 'Selamat anda mendapatkan sebesar Diskon 10%'},
+          'TIKTOK55': { discount: 15, type: 'percentage', description: 'Selamat anda mendapatkan sebesar Diskon 15%'},
+          'LAZADA66': { discount: 25, type: 'percentage', description: 'Selamat anda mendapatkan sebesar Diskon 25%'},
+          'LAZADA55': { discount: 10, type: 'percentage', description: 'Selamat anda mendapatkan sebesar Diskon 10%'},
+          'KFCMURAH': { discount: 8, type: 'percentage', description: 'Selamat anda mendapatkan sebesar Diskon 8%'},
+        };
+        
         // Mapping metode pembayaran dengan warna
         const paymentMethodColors = {
             'transfer': 'bg-blue-100 text-blue-800',
